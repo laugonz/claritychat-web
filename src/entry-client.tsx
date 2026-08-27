5b4670fdebf1669a3e2d2dcf47e930386245c77c
Laura González <lgonzalez@civitatis.com>
2026-08-27 13:51:35 +0200

    Add Vercel Web Analytics

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { findRoute } from "./routes";
 import "../public/style.css";
 
 const root = document.getElementById("root");
-if (root) createRoot(root).render(findRoute(window.location.pathname).element);
+if (root) {
+  createRoot(root).render(
+    <>
+      {findRoute(window.location.pathname).element}
+      <Analytics />
+    </>,
+  );
+}
